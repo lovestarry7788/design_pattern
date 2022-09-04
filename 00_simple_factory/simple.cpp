55389ee52dbfcd3e07e4748adf52c7419c847918
lovestarry7788 <jay.lau2020.work@outlook.com>
2022-09-04 18:04:25 +0800

    factory

---
+++ 00_simple_factory/simple.cpp
@@ -0,0 +1,59 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class API {
+public:
+    virtual ~API() {}
+    virtual void Say() = 0;
+};
+
+class Hello : public API{
+public:
+    void Say() { cout << "Hello" << endl; }
+};
+
+class Hi : public API {
+public:
+    void Say() { cout << "Hi" << endl;}
+};
+
+enum ProductType {
+    HI,
+    HELLO
+};
+
+class Factory {
+public:
+    API* CreateAPI(ProductType product) {
+        switch(product) {
+            case HI : {
+                return new Hi();
+            }
+            case HELLO : {
+                return new Hello();
+            }
+            default:
+                return nullptr;
+        }
+    } 
+};
+
+int main(int argc, char const *argv[]) {
+    Factory factory;
+    API *Hi = factory.CreateAPI(HI);
+    if(Hi != nullptr) {
+        Hi -> Say();
+        delete Hi;
+    }
+    
+    API *Hello = factory.CreateAPI(HELLO);
+    if(Hello != nullptr) {
+        Hello -> Say();
+        delete Hello;
+    }
+    
+    return 0;
+}
